c09d71658062f28e6114a550d949bc325643da05
MahmoudAhmadOsman <mahmoudosman.som@gmail.com>
2020-01-05 00:48:39 -0600

    =added couple string examples and string methods

---
+++ src/com/strings/StringAndMethods.java
@@ -0,0 +1,20 @@
+package com.strings;
+
+public class StringAndMethods {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String name = "Mahmoud Osman";
+		String degree = "Bacholer's degree";
+		String schoolName = "Champlain College";
+		int year = 2019;
+		
+		System.out.println("My name is " + name);
+		System.out.println("I have a " + degree.toUpperCase() + " in Computer Science");
+		System.out.println("I graduated from " + schoolName.toUpperCase());
+		System.out.println("I got my degree in " + year);
+		
+	}
+
+}
