46e0abd2e578997ef573dd33834b532f63195100
MahmoudAhmadOsman <mahmoudosman.som@gmail.com>
2020-01-04 23:14:45 -0600

    added arrays, for loop  and foreach

---
+++ src/ifStatement/ForEach.java
@@ -0,0 +1,29 @@
+package ifStatement;
+
+public class ForEach {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] Numbers = {17, 32, 45, 96,85, 655};
+		
+		for(int n : Numbers) {
+			  System.out.println(Numbers);
+		}
+
+		
+//Print some names and loop through them
+		
+String[] Names = {"Name 1 ", "Name 2", "Name 3 " , "Name 4"};
+		
+		for(String name : Names) {
+			  System.out.println(name);
+		}
+
+		
+		
+		
+	}
+
+}
+   
---
+++ src/ifStatement/WhileLoop.java
@@ -0,0 +1,25 @@
+package ifStatement;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		
+		//print values from 1 to 12
+		
+		int a = 1;
+			while(a <= 12) {
+				System.out.println("a value is now => " + "[" + a + "]");
+				a++;
+		}
+
+	// for loop does the same
+			
+			System.out.println("_______________For Loop__________________");
+			
+			for(int i = 0; i <=12; i++) {
+				System.out.println("i is now " + "[" + i + "]");
+			}
+			
+	}
+
+}
