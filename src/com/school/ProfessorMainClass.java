99730be360cb59c41b92de1ece84d90a7dd8d6c2
MahmoudAhmadOsman <mahmoudosman.som@gmail.com>
2020-01-24 02:44:40 -0600

    added inheritance class

---
+++ src/com/school/ProfessorMainClass.java
@@ -0,0 +1,13 @@
+package com.school;
+
+public class ProfessorMainClass {
+
+	public static void main(String[] args) {
+		Professor James = new Professor("James", "Daivs \n", "Phone:\n 802-354-0000 \n",
+				"Address: \n 123 Main St. Blain,  MN 55435.");
+
+		James.print();
+
+	}
+
+}
